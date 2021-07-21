1651e3cf584d040b4e68c8db8ff8799706651661
Marlon Hildon <marlonhildon@gmail.com>
2021-07-21 12:02:10 -0300

    Initial architecture

---
+++ src/main/java/br/com/mh/csv/Application.java
@@ -0,0 +1,13 @@
+package br.com.mh.csv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
